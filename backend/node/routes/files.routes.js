ed1145292a65db00caac91f83b441a33bedfbb68
Luis César Lizama Quiñónez <DerLuisLizama@gmail.com>
2025-03-31 22:57:19 -0600

    feat: implement file retrieval functionality

--- backend/node/routes/files.routes.js
+++ backend/node/routes/files.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const authenticateToken = require("../middlewares/authenticateToken");
-const {uploadFile} = require("../controllers/files.controller");
+const {getFiles, uploadFile} = require("../controllers/files.controller");
 
 const router = express.Router();
 
+router.get("/", authenticateToken, getFiles);
 router.post("/", authenticateToken, uploadFile);
 
 module.exports = router;
